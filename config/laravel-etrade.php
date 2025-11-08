f901481f30c742691fc60235594689a3fd90e383
Kevin Rider <github@thebackwaters.com>
2025-11-08 16:37:25 -0700

    switch env -> production

--- config/laravel-etrade.php
+++ config/laravel-etrade.php
@@ -4,11 +4,11 @@ return [
     'app_key' => env('ETRADE_APP_KEY'),
     'app_secret' => env('ETRADE_APP_SECRET'),
     /*
-    |--------------------------------------------------------------------------
+    |---------------------------
     | E*Trade API environment
-    |--------------------------------------------------------------------------
-    | options: 'sandbox' or 'prod'
-    | Start with the sandbox first if you are new to the API.
+    |---------------------------
+    | false: sandbox
+    | true: prod
     */
-    'env' => 'sandbox',
+    'production' => false,
 ];
