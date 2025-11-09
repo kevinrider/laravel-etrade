7eab834dab45a6f13075db9403374655c252b036
Kevin Rider <github@thebackwaters.com>
2025-11-09 12:34:36 -0700

    add EtradeAccessTokenDTO

---
+++ src/Dtos/EtradeAccessTokenDTO.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace KevinRider\LaravelEtrade\Dtos;
+
+use Illuminate\Support\Carbon;
+
+class EtradeAccessTokenDTO extends BaseDTO
+{
+    public string $oauthToken;
+    public string $oauthTokenSecret;
+    public Carbon $inactiveAt;
+}
