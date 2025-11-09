f4714b5c2ce828b554d416f79163590649970ef4
Kevin Rider <github@thebackwaters.com>
2025-11-09 10:54:08 -0700

    add oauth_access_token_key to config

--- config/laravel-etrade.php
+++ config/laravel-etrade.php
@@ -3,6 +3,8 @@
 return [
     'app_key' => env('ETRADE_APP_KEY'),
     'app_secret' => env('ETRADE_APP_SECRET'),
+    'oauth_request_token_key' => 'laravel_etrade_oauth_request_token',
+    'oauth_access_token_key' => 'laravel_etrade_oauth_access_token',
     /*
     |---------------------------
     | E*Trade API environment
@@ -11,5 +13,4 @@ return [
     | true: prod
     */
     'production' => false,
-    'oauth_request_token_key' => 'laravel_etrade_oauth_request_token',
 ];
