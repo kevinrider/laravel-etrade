c49c3d3d7492ed2b4bbe1eb8eaf040f508d81ea1
Kevin Rider <github@thebackwaters.com>
2025-11-09 12:38:24 -0700

    add inactive_buffer_in_seconds to config

--- config/laravel-etrade.php
+++ config/laravel-etrade.php
@@ -6,6 +6,12 @@ return [
     'oauth_request_token_key' => 'laravel_etrade_oauth_request_token',
     'oauth_access_token_key' => 'laravel_etrade_oauth_access_token',
     /*
+     * access tokens are good for up to 7200 seconds of inactivity
+     * renew access token when inactive_buffer_in_seconds or less
+     * time remains.
+     */
+    'inactive_buffer_in_seconds' => 1800,
+    /*
     |---------------------------
     | E*Trade API environment
     |---------------------------
