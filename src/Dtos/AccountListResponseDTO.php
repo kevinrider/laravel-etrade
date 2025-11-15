a21285c868c192dc22f949ae163c5ec17b46647b
Kevin Rider <github@thebackwaters.com>
2025-11-15 15:28:39 -0700

    override BaseDTO:fill() to handle nesting in AccountListResponseDTO

--- src/Dtos/AccountListResponseDTO.php
+++ src/Dtos/AccountListResponseDTO.php
@@ -8,4 +8,21 @@ class AccountListResponseDTO extends BaseDTO
      * @var AccountDTO[]
      */
     public array $accounts;
+
+    /**
+     * @param array $data
+     * @return void
+     */
+    protected function fill(array $data): void
+    {
+        $this->accounts = [];
+        if (isset($data['Accounts']['Account'])) {
+            $accountsData = $data['Accounts']['Account'];
+            // Handle case where there is only one account and simplexml doesn't create a nested array
+            if (isset($accountsData['accountId'])) {
+                $accountsData = [$accountsData];
+            }
+            $this->accounts = array_map(fn($accountData) => new AccountDTO($accountData), $accountsData);
+        }
+    }
 }
