3aed01951f7206bea2545255222d0bf24063318b
Kevin Rider <github@thebackwaters.com>
2025-11-15 17:32:47 -0700

    setup AccountBalanceRequestDTO

---
+++ src/Dtos/Request/AccountBalanceRequestDTO.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KevinRider\LaravelEtrade\Dtos\Request;
+
+use KevinRider\LaravelEtrade\Dtos\BaseDTO;
+
+class AccountBalanceRequestDTO extends BaseDTO
+{
+    const array ALLOWED_QUERY_PARAMS = ['instType', 'realTimeNAV', 'accountType'];
+    public string $accountType;
+    public string $instType = 'BROKERAGE';
+    public string $realTimeNAV;
+    public string $accountIdKey;
+}
