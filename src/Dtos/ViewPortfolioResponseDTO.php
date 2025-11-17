68255ceb66174d9d55e13a8534e8a26acd0a3f9a
Kevin Rider <github@thebackwaters.com>
2025-11-16 19:50:43 -0700

    add ViewPortfolioResponseDTO

---
+++ src/Dtos/ViewPortfolioResponseDTO.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace KevinRider\LaravelEtrade\Dtos;
+
+use KevinRider\LaravelEtrade\Dtos\ViewPortfolio\PositionDTO;
+
+class ViewPortfolioResponseDTO extends BaseDTO
+{
+    public ?int $accountId = null;
+    public ?string $next = null;
+    public ?int $totalPages = null;
+    public ?int $nextPageNo = null;
+    /**
+     * @var PositionDTO[]
+     */
+    public array $positions = [];
+
+    /**
+     * @param array $data
+     * @return void
+     */
+    protected function fill(array $data): void
+    {
+        $data = $data['AccountPortfolio'] ?? $data['accountPortfolio'] ?? null;
+        if ($data) {
+            $positions = $data['Position'] ?? $data['position'] ?? null;
+            $positions = $this->isMultiDimensional($positions) ? $positions : [$positions];
+            $this->positions = array_map(fn ($accountPortfolio) => new PositionDTO($accountPortfolio), $positions);
+            unset($data['Position'], $data['position']);
+        }
+
+        parent::fill($data);
+    }
+
+    /**
+     * @param array $array
+     * @return bool
+     */
+    protected function isMultiDimensional(array $array): bool {
+        foreach($array as $element) {
+            if(!is_array($element)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
