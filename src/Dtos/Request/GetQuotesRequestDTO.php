085adfe13024bbb3c8fc59ed1b6f2e9a63ae8f6b
Kevin Rider <github@thebackwaters.com>
2025-11-22 14:55:53 -0700

    create GetQuotesRequestDTO

---
+++ src/Dtos/Request/GetQuotesRequestDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace KevinRider\LaravelEtrade\Dtos\Request;
+
+use KevinRider\LaravelEtrade\Dtos\BaseDTO;
+
+class GetQuotesRequestDTO extends BaseDTO
+{
+    public const array ALLOWED_QUERY_PARAMS = [
+        'detailFlag',
+        'requireEarningsDate',
+        'overrideSymbolCount',
+        'skipMiniOptionsCheck',
+    ];
+
+    public ?string $symbols = null;
+    public ?string $detailFlag = null;
+    public ?bool $requireEarningsDate = null;
+    public ?bool $overrideSymbolCount = null;
+    public ?bool $skipMiniOptionsCheck = null;
+}
