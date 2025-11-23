047cd29b0f9832df839f183e9029da4bd4c93c0b
Kevin Rider <github@thebackwaters.com>
2025-11-23 11:31:38 -0700

    setup option quote response dtos

---
+++ src/Dtos/OptionExpireDateResponseDTO.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace KevinRider\LaravelEtrade\Dtos;
+
+use KevinRider\LaravelEtrade\Dtos\Options\ExpirationDateDTO;
+
+class OptionExpireDateResponseDTO extends BaseDTO
+{
+    /**
+     * @var ExpirationDateDTO[]
+     */
+    public array $expirationDates = [];
+
+    /**
+     * @param array $data
+     * @return void
+     */
+    protected function fill(array $data): void
+    {
+        $expirationDates = $data['expirationDates'] ?? $data['ExpirationDates'] ?? $data['ExpirationDate'] ?? $data['expirationDate'] ?? null;
+        if ($expirationDates !== null) {
+            $this->expirationDates = array_map(
+                fn ($expirationDate) => new ExpirationDateDTO($expirationDate),
+                $this->normalizeExpirationDatesArray($expirationDates)
+            );
+
+            unset($data['expirationDates'], $data['ExpirationDates'], $data['ExpirationDate'], $data['expirationDate']);
+        }
+
+        parent::fill($data);
+    }
+
+    /**
+     * @param mixed $expirationDates
+     * @return array
+     */
+    private function normalizeExpirationDatesArray(mixed $expirationDates): array
+    {
+        if (!is_array($expirationDates) || empty($expirationDates)) {
+            return [];
+        }
+
+        if (array_keys($expirationDates) !== range(0, count($expirationDates) - 1)) {
+            return [$expirationDates];
+        }
+
+        return $expirationDates;
+    }
+}
